5317c7ea823064a555b5d7a680e4f0e120429623
isrealkayode57-svg <isrealkayode57@gmail.com>
2026-09-05 16:16:33 -0500

    Refactor GroceryIngredientRepository interface methods

--- YourMeal/src/main/java/com/softdev/yourmeal/GroceryIngredientRepository.java
+++ YourMeal/src/main/java/com/softdev/yourmeal/GroceryIngredientRepository.java
@@ -1,11 +1,16 @@
 package com.softdev.yourmeal;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroceryIngredientRepository extends JpaRepository<GroceryIngredient, Long> {
+public interface GroceryIngredientRepository
+        extends JpaRepository<GroceryIngredient, Long> {
 
     List<GroceryIngredient> findByUser(AppUser user);
 
-    boolean existsByUserAndNormalizedName(AppUser user, String normalizedName);
+    boolean existsByUserAndNormalizedName(
+            AppUser user,
+            String normalizedName
+    );
 }
