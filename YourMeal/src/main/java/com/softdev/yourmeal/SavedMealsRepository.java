3b185178f9e833d9218c97d305c24578029cef5e
isrealkayode57-svg <isrealkayode57@gmail.com>
2026-09-07 20:17:21 -0500

    Refactor SavedMealsRepository interface

--- YourMeal/src/main/java/com/softdev/yourmeal/SavedMealsRepository.java
+++ YourMeal/src/main/java/com/softdev/yourmeal/SavedMealsRepository.java
@@ -1,16 +1,11 @@
 package com.softdev.yourmeal;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SavedMealsRepository
-        extends JpaRepository<SavedMeals, Long> {
+public interface SavedMealsRepository extends JpaRepository<SavedMeals, Long> {
 
     List<SavedMeals> findByUser(AppUser user);
 
-    boolean existsByUserAndMealNames(
-            AppUser user,
-            String mealNames);
+    boolean existsByUserAndMealNames(AppUser user, String mealNames);
 }
-```
