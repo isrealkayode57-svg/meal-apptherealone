ed3faff902bce8682d8061f47ea246d154cb02e0
isrealkayode57-svg <isrealkayode57@gmail.com>
2026-09-13 20:37:18 -0500

    Change endpoint for saving actual meals

--- YourMeal/src/main/java/com/softdev/yourmeal/MainController.java
+++ YourMeal/src/main/java/com/softdev/yourmeal/MainController.java
@@ -391,7 +391,7 @@ public class MainController {
     // MealRecommendation object instead of only a plain string.
     // ============================================================
 
-    @PostMapping("/dashboard/actualmeals")
+    @PostMapping("/dashboard/actualmeals/save")
     public String saveMeals(
             HttpSession session,
             @RequestParam(name = "mealIndex", required = false)
