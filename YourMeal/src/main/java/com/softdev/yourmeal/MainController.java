a995c860b82e2e9beb3d5381dbca58cc6a96805e
isrealkayode57-svg <isrealkayode57@gmail.com>
2026-09-13 20:44:04 -0500

    Update MainController.java

--- YourMeal/src/main/java/com/softdev/yourmeal/MainController.java
+++ YourMeal/src/main/java/com/softdev/yourmeal/MainController.java
@@ -391,7 +391,7 @@ public class MainController {
     // MealRecommendation object instead of only a plain string.
     // ============================================================
 
-    @GetMapping("/dashboard/actualmeals/save")
+    @PostMapping("/dashboard/meals/save")
     public String saveMeals(
             HttpSession session,
             @RequestParam(name = "mealIndex", required = false)
